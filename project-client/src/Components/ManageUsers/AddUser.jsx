19aec4d7001749b1b7fbd80fcc87aab084859bf3
Noy Moshkovitz <noynava1@gmail.com>
2019-08-19 09:41:42 +0300

    Updated client's pckg-lock+addUser

--- project-client/src/Components/ManageUsers/AddUser.jsx
+++ project-client/src/Components/ManageUsers/AddUser.jsx
@@ -2,7 +2,52 @@ import React, { Component, Fragment } from 'react';
 import { send } from '../../serverUtils';
 import {pick} from "lodash";
 import './ManageUsers.scss';
-import { positions, gender, precent, salary, review, experience } from '../../resources';
+//import { positions, gender, precent, salary, review, experience } from '../../resources';
+
+const customerFields = ["name", "surname", "username", "password", "email", "position"];
+const managerFields = [...customerFields, "gender", "salary", "experience", "phone", "precent", "hiredAt", "active"]
+const employeeFields = [...managerFields, "review"];
+const positions = {
+    values: ["manager", "employee", "customer"],
+    default: "customer",
+    customer: {
+        fields: customerFields
+    },
+    manager: {
+        fields: managerFields
+    },
+    employee: {
+        fields: employeeFields
+    }
+
+};
+
+const gender = {
+    values: ["male", "female"],
+    default: "female"
+};
+
+const experience = {
+    values: ["1-5 years", "5-10 years", "over 10 years"],
+    default: "1-5 years"
+};
+
+const review = {
+    values: ['perfect', 'sufficient', 'insufficient'],
+    default: "sufficient"
+};
+
+const salary = {
+    min: 30,
+    default: 70
+};
+
+const precent = {
+    min: 10,
+    max: 100,
+    default: 100
+};
+
 
 class AddUser extends Component {
     constructor(props) {
